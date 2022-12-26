8f4ae44184951ce6751ab96fc8d927b386888af3
talalter <64363268+talalter@users.noreply.github.com>
2022-12-26 19:27:53 +0200

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -18,30 +18,30 @@ def return_path(node_state):
     while current_state is not None:
         path.append(current_state.action)
         current_state = current_state.parent
-    return path[::-1]
+    return path[::-1][1:]
 
 
-def minimax_decision(agent, state, world, turn_of_max, depth=6, alpha=float('-inf'), beta=float('inf')):
+def minimax_decision(agent1, agent2, state, world, turn_of_max, depth=6, alpha=float('-inf'), beta=float('inf')):
     if is_terminal_state(state) or depth == 0:
         return state.get_score(), state
     people_list, broken_list, location_tuple, scores_tuple = state.get_info()
     if turn_of_max:
         max_eval = float('-inf'), None
-        for son in world.get_neighbors(world.agent_locations[agent.id_]):
+        for son in world.get_neighbors(location_tuple[agent1.id_]):
             son_people_list = people_list.copy()
             son_broken_list = broken_list.copy()
             son_location_tuple = location_tuple.copy()
             son_scores_tuple = scores_tuple.copy()
             if not broken_list[son.id_]:
-                # action = TraverseAction(agent, son)
+                action = TraverseAction(agent1, son)
                 if son.is_brittle:
                     broken_list[son.id_] = True
                 son_scores_tuple[0] += son_people_list[son.id_]
                 son_people_list[son.id_] = 0
                 son_location_tuple[0] = son
                 new_state = StateNode(son_people_list, son_broken_list, son_scores_tuple,
-                                      son_location_tuple, parent=state)
-                eval_score, rec_state = minimax_decision(agent, new_state, world, False, depth - 1)
+                                      son_location_tuple, parent=state, action=action)
+                eval_score, rec_state = minimax_decision(agent1, agent2, new_state, world, False, depth - 1)
                 max_eval = max_eval if max_eval[0] >= eval_score else (eval_score, rec_state)
                 alpha = max(alpha, eval_score)
                 if beta <= alpha:
@@ -49,21 +49,21 @@ def minimax_decision(agent, state, world, turn_of_max, depth=6, alpha=float('-in
         return max_eval
     else:
         min_eval = float('inf'), None
-        for son in world.get_neighbors(location_tuple[agent.id_ % 2]):
+        for son in world.get_neighbors(location_tuple[agent2.id_]):
             son_people_list = people_list.copy()
             son_broken_list = broken_list.copy()
             son_location_tuple = location_tuple.copy()
             son_scores_tuple = scores_tuple.copy()
             if not broken_list[son.id_]:
-                # action = TraverseAction(self.B, son)
+                action = TraverseAction(agent2, son)
                 if son.is_brittle:
                     broken_list[son.id_] = True
                 son_scores_tuple[1] += son_people_list[son.id_]
                 son_people_list[son.id_] = 0
                 son_location_tuple[1] = son
                 new_state = StateNode(son_people_list, son_broken_list, son_scores_tuple,
-                                      son_location_tuple, parent=state)
-                eval_score, rec_state = minimax_decision(new_state, True, depth - 1)
+                                      son_location_tuple, parent=state, action=action)
+                eval_score, rec_state = minimax_decision(agent1, agent2, new_state, world, True, depth - 1)
                 min_eval = min_eval if min_eval[0] <= eval_score else (eval_score, rec_state)
                 beta = min(beta, eval_score)
                 if beta <= alpha:
@@ -118,11 +118,16 @@ class AdversarialAgent(Agent):
 
     def search(self):
         node_state = generate_state(self.state)
-        temp, n = minimax_decision(self, node_state, self.state.percept, True, self.l)
-        print(return_path(n))
+        other_agent = None
+        for agent in self.state.percept.agents:
+            if agent != self:
+                other_agent = agent
+        score, path_nodes = minimax_decision(self, other_agent, node_state, self.state.percept, True, 2)
+        actions_to_leaf = return_path(path_nodes)
         # seq = list(map(lambda x: TraverseAction(self, x.node, True), temp))
         # self.state.time += n * self.t
-        return n
+        # print(actions_to_leaf)
+        return actions_to_leaf
 
 
 class SemiCooperativeAgent(Agent):
