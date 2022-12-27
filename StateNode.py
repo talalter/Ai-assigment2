ddc62150707241a37a4807c24c3d8326e980db31
talalter <64363268+talalter@users.noreply.github.com>
2022-12-27 14:29:16 +0200

    Update StateNode.py

--- StateNode.py
+++ StateNode.py
@@ -6,15 +6,25 @@ class StateNode:
         self.parent = parent
         self.scores_tuple = scores_tuple
         self.location_tuple = location_tuple
+        self.action = action
 
-    def get_score(self):
+    def get_score_minimax(self):
         return self.scores_tuple[0]-self.scores_tuple[1]
+
+    def get_score_semi(self):
+        return list((self.scores_tuple[0], self.scores_tuple[1]))
+
+    def get_score_fully(self):
+        return self.scores_tuple[0] + self.scores_tuple[1]
 
     def set_parent(self, other):
         self.parent = other
 
     def get_info(self):
         return self.people_list, self.broken_list, self.location_tuple, self.scores_tuple
+
+    def __hash__(self):
+        return hash((tuple(self.people_list), tuple(self.broken_list)))
 
     # def __lt__(self, other):
     #     return self.node.id_ < other.node.id_
