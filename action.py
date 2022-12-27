4384619b1e0592482d6ff6bfab57263f343a02d1
talalter <64363268+talalter@users.noreply.github.com>
2022-12-27 14:34:17 +0200

    Create action.py

--- action.py
+++ action.py
@@ -2,7 +2,7 @@ class Action:
     def __init__(self, agent):
         self.target_vertex = agent.state.current_vertex
         self.agent = agent
-        self.graph = agent.world
+        self.graph = agent.state.percept
 
     def __call__(self):
         raise NotImplementedError
@@ -11,18 +11,14 @@ class Action:
         return self.__class__.__name__
 
     def move(self, pickup):
-        current_location = self.agent.current_vertex
         self.graph.change_agent_location(self.agent, self.target_vertex)
         if pickup:
-            self.agent.score += self.target_vertex.people
+            self.agent.state.people_saved += self.target_vertex.people
             self.graph.total_number_of_people_evacuated += self.target_vertex.people
-            self.target_vertex = self.target_vertex.take_people()
+            self.target_vertex.people = 0
         if self.target_vertex.is_brittle:
             self.target_vertex.is_broken = True
-        self.agent.time += 1
-        print(f"Agant {self.agent.id_} has moved from {current_location} to {self.target_vertex} with score "
-              f"{self.agent.score}")
-        print(self.graph)
+        self.agent.state.time += 1
 
 
 class TraverseAction(Action):
@@ -34,24 +30,21 @@ class TraverseAction(Action):
     def __call__(self):
         if self.graph.agent_locations[self.agent.id_] == self.target_vertex:
             raise Exception(F"this is is traverse action, why {self.agent} is moving to him self?")
-        #
-        # for neighbor in self.graph.get_neighbors(self.agent.state.current_vertex):
-        #     if neighbor == self.target_vertex:
         self.move(self.pickup)
         self.agent.current_vertex = self.target_vertex
-        # self.agent.state.time += linked_vertexes1[0][1]
         return False
 
 
 class NoOpAction(Action):
-    def __init__(self, agent, pickup):
+    def __init__(self, agent, pickup=True):
         super().__init__(agent)
         self.pickup = pickup
 
     def __call__(self):
-        self.agent.time += 1
+        self.agent.state.time += 1
         self.move(self.pickup)
         return False
+
 
 class TerminateAction(Action):
     def __init__(self, agent):
@@ -59,3 +52,4 @@ class TerminateAction(Action):
 
     def __call__(self):
         return True
+
